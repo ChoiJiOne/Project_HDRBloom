b39f4141f63798b9eabf5cea8e6087e3d608dc75
CHOIJIONE\choij <choijione02@gmail.com>
2024-04-27 13:13:09 +0900

    사용할 모듈 초기화/해제 및 게임 루프 추가

--- HDRBloom/Src/Main.cpp
+++ HDRBloom/Src/Main.cpp
@@ -1,7 +1,30 @@
 #include <cstdint>
 #include <Windows.h>
 
+#include "CrashModule.h"
+#include "PlatformModule.h"
+#include "RenderModule.h"
+
 int32_t WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR pCmdLine, _In_ int32_t nCmdShow)
 {
+	PlatformModule::WindowConstructParams windowParam{ L"HDRBloom", 100, 100, 1000, 800, false, false };
+
+	CrashModule::Init();
+	PlatformModule::Init(windowParam);
+	RenderModule::Init(PlatformModule::GetWindowHandle());
+
+	PlatformModule::SetEndLoopCallback([&]() { RenderModule::Uninit(); });
+
+	PlatformModule::RunLoop(
+		[&](float deltaSeconds)
+		{
+			RenderModule::BeginFrame(0.0f, 0.0f, 0.0f, 1.0f);
+
+			RenderModule::EndFrame();
+		}
+	);
+
+	PlatformModule::Uninit();
+	CrashModule::Uninit();
 	return 0;
 }
