892011a62097f448d6357ec67427c3e51d550180
CHOIJIONE\choij <choijione02@gmail.com>
2024-04-27 13:09:02 +0900

    빈 메인 함수 추가

---
+++ HDRBloom/Src/Main.cpp
@@ -0,0 +1,7 @@
+#include <cstdint>
+#include <Windows.h>
+
+int32_t WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR pCmdLine, _In_ int32_t nCmdShow)
+{
+	return 0;
+}
